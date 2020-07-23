9f11f06ed6637af57a09ff9f0ad3edce69054b0a
unknown <joaoaugustosn@gmail.com>
2020-07-23 07:27:45 -0300

    criando-arrays

---
+++ Javascript/empresa.js
@@ -0,0 +1,9 @@
+const empresa = {
+    nome: "Rocketseat",
+    cor: "Roxo",
+    foco: "Programação",
+    endereco: "Rua Guilherme Gembala",
+    numero: "260"
+}
+
+console.log(`A empresa ${empresa.nome} está localizada em ${empresa.endereco}, ${empresa.numero}`)
